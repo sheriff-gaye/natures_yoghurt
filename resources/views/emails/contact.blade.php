f703960e3142d9442b1aa938220bdc512da3f27a
sheriff-gaye <sheriffgaye5@gmail.com>
2022-12-02 23:46:21 +0000

    hello

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,13 +1,15 @@
-
 @extends('layouts.checkout')
 
-
 @section('checkout-content')
-
-
+<h3>Dear Natures Yogurt,</h3>
+<p>This is a message sent to you from the Nature's Yogurt website. Look at the information and respond as soon as you can.
+</p>
+<h3>Message Information</h3>
 <p>Name:{{ $Name }}</p>
 <p>Email: {{ $Email }}</p>
 <p>Message: {{ $Message }}</p>
 
+<h5>Best regards,</h5>
+<h5>{{ $Name }}</h5>
 
 @endsection

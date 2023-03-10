c59865d1903104a18b0586005e093db30c84a5de
sheriff-gaye <sheriffgaye5@gmail.com>
2023-03-10 20:27:21 +0000

    all

--- resources/views/livewire/product-tables.blade.php
+++ resources/views/livewire/product-tables.blade.php
@@ -1,20 +1,7 @@
 <div>
     <link rel="stylesheet" href="{{ asset('css/shop.css') }}">
-    {{-- <section class="search_bar">
-        <div class="container search_bar-container">
-            <div>
-                <i class="uil uil-search"></i>
-                <input type="search" id="search-item" wire:model.debounce='search' placeholder="Search Product">
-
-            </div>
-        </div>
-    </section> --}}
-
-
-
 
     <section class="products">
-        {{-- <h2>Our Products</h2> --}}
         <div class="container products_container">
 
             @foreach ($products as $product)
@@ -34,19 +21,18 @@
                         @endif
                     </div>
                 </article>
-
             @endforeach
         </div>
     </section>
 
 
-    @if($products->count()<1)
-    <section style="text-align:center;margin-top:-10rem">
-     <div class="container">
-         <h4>Oops, No Product Found! </h4>
-         <h4>Try Again<i class="uil uil-sad"></i></h4>
-     </div>
-    </section>
+    @if ($products->count() < 1)
+        <section style="text-align:center;margin-top:-10rem">
+            <div class="container">
+                <h4>Oops, No Product Found! </h4>
+                <h4>Try Again<i class="uil uil-sad"></i></h4>
+            </div>
+        </section>
     @endif
 
 
